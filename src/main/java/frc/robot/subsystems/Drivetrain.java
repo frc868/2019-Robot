fba93ad255360d2489d2e11a3624d0ec2d75d1d2
Armaan Goel <armaangoel78@gmail.com>
2019-01-12 17:07:18 -0500

    Added javadocing

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -21,56 +21,100 @@ public class Drivetrain {
         rightSecondary.follow(rightPrimary);
     }
 
+    /**
+     * sets left motor's speed
+     * @param speed percentage power from -1 to 1
+     */
     public void setLeftSpeed(double speed) {
         leftPrimary.set(Helper.boundValue(speed));
     }
 
+     /**
+     * sets right motor's speed
+     * @param speed percentage power from -1 to 1
+     */
     public void setRightSpeed(double speed) {
         rightPrimary.set(Helper.boundValue(speed));
     }
 
+     /**
+     * sets  motors' speed
+     * @param leftSpeed left's percentage power from -1 to 1
+     * @param rigthSpeed right's percentage power from -1 to 1
+     */
     public void setSpeed(double leftSpeed, double rightSpeed) {
         setLeftSpeed(leftSpeed);
         setRightSpeed(rightSpeed);
     }
 
+    /**
+     * turn off both motors
+     */
     public void off() {
         leftPrimary.stopMotor();
         rightPrimary.stopMotor();
     }
 
+    /**
+     * 
+     * @return speed left motor is set to
+     */
     public double getLeftSpeed() {
         return leftPrimary.get();
     }
 
+    /**
+     * 
+     * @return speed right motor is set to
+     */
     public double getRightSpeed() {
         return rightPrimary.get();
     }
 
-    public double getSpeed() {
-        return (getLeftSpeed() + getRightSpeed())/2;
-    }
-
+    /**
+     * 
+     * @return position of left motor according to encoder
+     */
     public double getLeftEncPosition() {
         return leftPrimary.getEncoder().getPosition();
     }
 
+    /**
+     * 
+     * @return position of right motor according to encoder
+     */
     public double getRightEncPosition() {
         return rightPrimary.getEncoder().getPosition();
     }
 
+    /**
+     * 
+     * @return average position of motors according to encoder
+     */
     public double getEncPosition() {
         return (getLeftEncPosition() + getRightEncPosition())/2;
     }
 
+    /**
+     * 
+     * @return velocity of left motor according to encoder
+     */
     public double getLeftEncVelocity() {
         return leftPrimary.getEncoder().getVelocity();
     }
 
+    /**
+     * 
+     * @return velocity of right motor according to encoder
+     */
     public double getRightEncVelocity() {
         return rightPrimary.getEncoder().getVelocity();
     }
 
+    /**
+     * 
+     * @return average velocity of motors according to encoder
+     */
     public double getEncVelocity() {
         return (getLeftEncVelocity() + getRightEncVelocity())/2;
     }
