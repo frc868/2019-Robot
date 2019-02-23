60623400f7cd3a89fad8b9f69303967fea990653
Armaan Goel <armaangoel78@gmail.com>
2019-02-23 12:49:43 -0500

    added pov buttons, finished controller mappings

--- src/main/java/frc/robot/helpers/oi/POVButtonPlus.java
+++ src/main/java/frc/robot/helpers/oi/POVButtonPlus.java
@@ -1,13 +1,12 @@
 package frc.robot.helpers.oi;
 
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.buttons.POVButton;
 import frc.robot.helpers.commands.EmptyCommand;
 
-public class ButtonPlus extends JoystickButton {
+public class POVButtonPlus extends POVButton {
 
-    public ButtonPlus(XboxControllerPlus controller, int id) {
-        super(controller, id);
+    public POVButtonPlus(XboxControllerPlus controller, int angle) {
+        super(controller, angle);
     }
 
     public void resetMappings() {
@@ -21,5 +20,7 @@ public class ButtonPlus extends JoystickButton {
         toggleWhenActive(new EmptyCommand());
         toggleWhenPressed(new EmptyCommand());
     }
+
+
 
 }
