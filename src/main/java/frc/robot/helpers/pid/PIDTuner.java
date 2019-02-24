d8cfebe60ed40716cd89070e1d44b145f8bdfd21
Armaan Goel <armaangoel78@gmail.com>
2019-02-23 20:56:13 -0500

    Added namability to PIDTuner class

--- src/main/java/frc/robot/helpers/pid/PIDTuner.java
+++ src/main/java/frc/robot/helpers/pid/PIDTuner.java
@@ -4,26 +4,38 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class PIDTuner extends Command {
-    public PIDCommandPlus command;
-    public double P, I, D;
+    private PIDCommandPlus command;
+    private double P, I, D;
+    private String PKey = "P", IKey = "I", DKey = "D";
+
+    public PIDTuner(String name, PIDCommandPlus command) {
+        PKey = name + " " + PKey;
+        IKey = name + " " + IKey;
+        DKey = name + " " + DKey;
+        this.command = command;
+        setup();
+    }
 
     public PIDTuner(PIDCommandPlus command) {
         this.command = command;
+        setup();   
+    }
 
+    private void setup() {
         P = command.getP();
         I = command.getI();
         D = command.getD();
 
-        SmartDashboard.putNumber("P", P);
-        SmartDashboard.putNumber("I", I);
-        SmartDashboard.putNumber("D", D);
+        SmartDashboard.putNumber(PKey, P);
+        SmartDashboard.putNumber(IKey, I);
+        SmartDashboard.putNumber(DKey, D);
     }
 
     @Override
     protected void initialize() {
-        P = SmartDashboard.getNumber("P", P);
-        I = SmartDashboard.getNumber("I", I);
-        D = SmartDashboard.getNumber("D", D);
+        P = SmartDashboard.getNumber(PKey, P);
+        I = SmartDashboard.getNumber(IKey, I);
+        D = SmartDashboard.getNumber(DKey, D);
 
         command.setP(P);
         command.setI(I);
