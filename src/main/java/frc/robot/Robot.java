578a815f7ba60e09d28109488740f0aa4f708eb2
Tornado702 <ryansforrest@gmail.com>
2019-02-27 16:04:11 -0500

    updated wpilib and changed to drivetrainNEO

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,7 +17,7 @@ import frc.robot.climberelevator.footdrive.FootDrive;
 import frc.robot.climberelevator.forks.Forks;
 import frc.robot.climberelevator.powerpack.PowerPack;
 import frc.robot.climberelevator.ramps.Ramps;
-import frc.robot.drivetrain.Drivetrain;
+import frc.robot.drivetrain.DrivetrainNEO;
 import frc.robot.helpers.subsystems.SubsystemManager;
 import frc.robot.oi.OI;
 import frc.robot.sensors.camera.Camera;
