30bb302420e4a291a39e57029a8163a582b7a9b9
Killerskyman <kilerskyman@gmail.com>
2019-03-07 18:41:25 -0500

    Added slow drive for ArcadeDrive

--- src/main/java/frc/robot/oi/ArcadeDrive.java
+++ src/main/java/frc/robot/oi/ArcadeDrive.java
@@ -7,14 +7,23 @@ import frc.robot.helpers.oi.XboxControllerPlus;
 
 public class ArcadeDrive extends Command {
 
+    public boolean isSlow;
+
     public ArcadeDrive() {
+        this.isSlow = false;
         requires(Robot.drivetrain);
+    }
+
+    public ArcadeDrive(boolean isSlow){
+        this.isSlow = isSlow;
     }
 
     @Override
     protected void execute() {
         double y = -OI.driver.getLY();
         double x = -OI.driver.getRX();
+        y = isSlow ? (y * 0.25) : y;
+        x = isSlow ? (x * 0.25) : x;
         Robot.drivetrain.setSpeed(y - x, y + x);
     }
 
