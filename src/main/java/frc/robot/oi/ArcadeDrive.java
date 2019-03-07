d1c8da3a52f765f5a55cb603f5ef4e35382eb9ad
Dee <35465486+20iyerd@users.noreply.github.com>
2019-03-07 18:46:47 -0500

    cubed/scaled joystick inputs

--- src/main/java/frc/robot/oi/ArcadeDrive.java
+++ src/main/java/frc/robot/oi/ArcadeDrive.java
@@ -3,6 +3,7 @@ package frc.robot.oi;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.*;
+import frc.robot.helpers.Helper;
 import frc.robot.helpers.oi.XboxControllerPlus;
 
 public class ArcadeDrive extends Command {
@@ -13,8 +14,8 @@ public class ArcadeDrive extends Command {
 
     @Override
     protected void execute() {
-        double y = -OI.driver.getLY();
-        double x = -OI.driver.getRX();
+        double y = Math.pow(Helper.deadzone(-OI.driver.getLY(),0.1),3);
+        double x = Math.pow(Helper.deadzone(0.75*(-OI.driver.getRX()),0.1),3);
         Robot.drivetrain.setSpeed(y - x, y + x);
     }
 
