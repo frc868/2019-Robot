7366cac53d6c05bf65669d1cb63cb860802eb030
Tornado702 <ryansforrest@gmail.com>
2019-03-07 19:27:29 -0500

    Made slow drive while in climber mode

--- src/main/java/frc/robot/oi/OI.java
+++ src/main/java/frc/robot/oi/OI.java
@@ -57,7 +57,6 @@ public class OI {
     driver.menu.whenPressed(new SetFootDriveSpeed(0));
     driver.menu.whenPressed(new SetTiltPosition(Tilt.UPPER));
     driver.menu.whenPressed(new Grab());
-    driver.menu.whenPressed(new ArcadeDrive(true));
     driver.rb.and(driver.y).whenPressed(new AutoClimb(true));
 
     // driver.rb.and(driver.povN).pressToStartReleaseToStop(new SetClimberSpeed(0.5), new SetClimberSpeed(0));
