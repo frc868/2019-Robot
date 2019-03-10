6485a7259d0703fd26775cecb3d86839c7ac84ef
Armaan Goel <armaangoel78@gmail.com>
2019-03-09 19:57:31 -0500

    OI mappings based on Josh Moore's request

---
+++ src/main/java/frc/robot/oi/ManualClimber.java
@@ -0,0 +1,43 @@
+package frc.robot.oi;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.helpers.Helper;
+
+public class ManualClimber extends Command {
+
+    public ManualClimber() {
+        requires(Robot.powerPack);
+    }
+
+    @Override
+    protected void initialize() {
+        Robot.powerPack.switchToClimber();
+        Robot.powerPack.climberBrakeOff();
+    }
+
+    @Override
+    protected void execute() {
+        double input = Helper.deadzone(-OI.operator.getLY(), 0.1);
+        Robot.powerPack.setSpeed(input);
+
+        if(input == 0) { 
+            Robot.powerPack.elevatorBrakeOn();
+        } else {
+            Robot.powerPack.elevatorBrakeOff();
+        }
+
+        Robot.footDrive.setSpeed(-OI.operator.getRY());
+    }
+
+    @Override
+    protected void end() {
+        Robot.powerPack.stop();
+        Robot.powerPack.climberBrakeOn();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
--- src/main/java/frc/robot/oi/OI.java
+++ src/main/java/frc/robot/oi/OI.java
@@ -2,20 +2,20 @@ package frc.robot.oi;
 
 import frc.robot.Robot;
 import frc.robot.RobotMap;
-import frc.robot.carriage.hatchclaw.Grab;
+import frc.robot.carriage.ballintake.IntakeUntilBallDetected;
+import frc.robot.carriage.hatchclaw.GrabWhenDetected;
 import frc.robot.carriage.hatchclaw.ToggleClaw;
 import frc.robot.carriage.tilt.SetTiltPosition;
 import frc.robot.carriage.tilt.Tilt;
-import frc.robot.climberelevator.footdrive.SetFootDriveSpeed;
 import frc.robot.climberelevator.forks.DeployForks;
 import frc.robot.climberelevator.powerpack.AutoClimb;
 import frc.robot.climberelevator.powerpack.PowerPack;
 import frc.robot.climberelevator.powerpack.SetElevatorPosition;
 import frc.robot.climberelevator.ramps.DeployRamps;
 import frc.robot.helpers.oi.XboxControllerPlus;
+import frc.robot.sensors.camera.FollowVision;
 
 public class OI {
-
     public static XboxControllerPlus driver;
     public static XboxControllerPlus operator;
 
@@ -23,45 +23,41 @@ public class OI {
         driver = new XboxControllerPlus(RobotMap.Controls.DRIVER);
         operator = new XboxControllerPlus(RobotMap.Controls.OPERATOR);
 
-        //commands involving manual control and variable input
+        // MANUAL CONTROLS
         Robot.drivetrain.setDefaultCommand(new ArcadeDrive());
         Robot.ballIntake.setDefaultCommand(new ManualIntake());
-        Robot.tilt.setDefaultCommand(new ManualTilt());
-        Robot.powerPack.setDefaultCommand(new ManualPowerpack());  //powerpack so we can toggle elevator vs climber instead of just ManualElevator
+
+        // DRIVER CONTROLS 
+        driver.a.whileHeld(new FollowVision());
+        driver.b.pressToStartReleaseToStop(new GrabWhenDetected());
+        driver.x.pressToStartReleaseToStop(new IntakeUntilBallDetected());
+        driver.y.and(driver.rb).whenPressed(new AutoClimb(true));
+
+        driver.rb.whenPressed(new ManualClimber());
+        driver.lb.whenPressed(new ToggleClaw());
+
+        driver.start.whenPressed(new DeployForks());
+        driver.menu.whenPressed(new DeployRamps());
 
 
-
-        //MAIN OPERATOR CONTROLS
-        operator.povN.whenPressed(new SetTiltPosition(Tilt.UPPER));
-        operator.povE.whenPressed(new SetTiltPosition(Tilt.MIDDLE));
-        operator.povS.whenPressed(new SetTiltPosition(Tilt.LOWER));
-
+        // OPERATOR CONTROLS
         operator.a.whenPressed(new SetElevatorPosition(PowerPack.LOWER));
         operator.b.whenPressed(new SetElevatorPosition(PowerPack.MIDDLE));
         // operator.x.whenPressed(new SetElevatorPosition(PowerPack.INTAKE_BALL));  //TODO: fix PID so it doesn't slam into the bottom
         operator.y.whenPressed(new SetElevatorPosition(PowerPack.UPPER));
 
         operator.lb.whenPressed(new ToggleClaw());
+        operator.rb.whenPressed(new ManualTilt());
+        operator.rb.whenPressed(new ManualPowerpack());
 
-
-        //ENDGAME
-        driver.start.whenPressed(new DeployForks());
-        driver.menu.whenPressed(new DeployRamps());
-        driver.menu.whenPressed(new SetFootDriveSpeed(0));
-        driver.menu.whenPressed(new SetTiltPosition(Tilt.UPPER));
-        driver.menu.whenPressed(new Grab());
-        driver.rb.and(driver.y).whenPressed(new AutoClimb(true));
-
+        operator.povN.whenPressed(new SetTiltPosition(Tilt.UPPER));
+        operator.povE.whenPressed(new SetTiltPosition(Tilt.MIDDLE));
+        operator.povS.whenPressed(new SetTiltPosition(Tilt.LOWER));
 
         // STUFF WE'RE NOT USING RIGHT NOW
         // Operator GrountHatch control, not implemented on robot
         // operator.start.whenPressed(new OpenAndIntake());
         // operator.menu.whenPressed(new GiveToClaw());
-
-        // driver.a.whileHeld(new FollowVision());
-        // driver.b.pressToStartReleaseToStop(new GrabWhenDetected());
-        // driver.x.pressToStartReleaseToStop(new IntakeUntilBallDetected());
-
     }
 
     public static void update() {
