9f36b3253fb36689bf1501c4e82e0520deac6944
Armaan Goel <armaangoel78@gmail.com>
2019-03-17 18:30:49 -0400

    added constants for analog distance sensor

--- src/main/java/frc/robot/helpers/sensors/AnalogDistanceSensor.java
+++ src/main/java/frc/robot/helpers/sensors/AnalogDistanceSensor.java
@@ -4,14 +4,16 @@ import edu.wpi.first.wpilibj.AnalogInput;
 import frc.robot.helpers.sensors.AnalogDistanceLimit;
 
 public class AnalogDistanceSensor extends AnalogInput {
-    private final double SLOPE = 0, Y_INTERCEPT = 0;
+    private final double SLOPE, Y_INTERCEPT;
 
     public AnalogDistanceSensor(int port) {
         super(port);
+        SLOPE = 0.047;
+        Y_INTERCEPT = -7.8 * Math.pow(10, -3);
     }
 
     public double getDistance() {
-        return 1/(getVoltage()*SLOPE + Y_INTERCEPT);
+        return 1/(getVoltage() * SLOPE + Y_INTERCEPT);
     }
 
     public AnalogDistanceLimit getLimit(double LIMIT) {
