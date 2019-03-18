3659d6edebcf8465fd8d5228f6912f608a52baff
Armaan Goel <armaangoel78@gmail.com>
2019-03-17 22:18:50 -0400

    renaming, cleanup

--- src/main/java/frc/robot/climberelevator/powerpack/AutoLevelingSetClimberSpeed.java
+++ src/main/java/frc/robot/climberelevator/powerpack/AutoLevelingSetClimberSpeed.java
@@ -3,15 +3,15 @@ package frc.robot.climberelevator.powerpack;
 import frc.robot.Robot;
 import frc.robot.helpers.pid.PIDCommandPlus;
 
-public class LevelingSetClimberSpeed extends PIDCommandPlus {
+public class AutoLevelingSetClimberSpeed extends PIDCommandPlus {
     private double targetSpeed;
     private static final double P = 1.0, I = 0.0, D = 0.0;
 
-    public LevelingSetClimberSpeed(double targetSpeed) {
+    public AutoLevelingSetClimberSpeed(double targetSpeed) {
         this(targetSpeed, P, I, D);
     }
 
-    public LevelingSetClimberSpeed(double targetSpeed, double kP, double kI, double kD) {
+    public AutoLevelingSetClimberSpeed(double targetSpeed, double kP, double kI, double kD) {
         super(kP, kI, kD, 0);
         requires(Robot.powerPack);
         this.targetSpeed = targetSpeed;
--- src/main/java/frc/robot/climberelevator/powerpack/SetClimberSpeed.java
+++ src/main/java/frc/robot/climberelevator/powerpack/SetClimberSpeed.java
@@ -1,6 +1,6 @@
 package frc.robot.climberelevator.powerpack;
 
-public class SetClimberSpeed extends LevelingSetClimberSpeed {
+public class SetClimberSpeed extends AutoLevelingSetClimberSpeed {
     public SetClimberSpeed(double targetSpeed) {
         super(targetSpeed, 0, 0, 0);
     }
