5aea99a65667af5667bcbfbac7995bd2058dd199
Dee <35465486+20iyerd@users.noreply.github.com>
2019-03-21 18:45:28 -0400

    tried to fix sniper mode

--- src/main/java/frc/robot/oi/ArcadeDrive.java
+++ src/main/java/frc/robot/oi/ArcadeDrive.java
@@ -17,16 +17,18 @@ public class ArcadeDrive extends Command {
     protected void execute() {
         double y = Helper.deadzone(-OI.driver.getLY(), .03);
         double x = Helper.deadzone(-OI.driver.getRX(), .03);
-        if(!Robot.powerPack.isElevatorMode())   {       //slow for climbing
-            y = y*.25;
-            x = x*.25;
+        if(!Robot.powerPack.isElevatorMode()){       //slow for climbing
+            y = y * .25;
+            x = x * .25;
         }
 
-        if(OI.driver.rb.get())   {      //Josh's sniper button
-            if(x > 0)
-            x = Math.abs(Math.pow(x, 0.55555555555555555555));
-            if(x < 0)
-            x = -Math.abs(Math.pow(x, 0.55555555555555555555));
+        if(OI.driver.rb.get()){      //Josh's sniper button
+            if(x > 0){
+                x = Math.abs(Math.pow(x, 0.55555555555555555555));
+            }
+            if(x < 0){
+                x = -Math.abs(Math.pow(x, 0.55555555555555555555));
+            }
         }
         
         Robot.drivetrain.setSpeed(y - x, y + x);
