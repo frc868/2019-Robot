ae51332aef7e3c8016373ca742623ad55580e15e
tornado702 <ryansforrest@gmail.com>
2019-04-19 11:18:34 -0400

    created right rocket 1 hatch auton path

---
+++ src/main/java/frc/robot/auton/paths/RightRocketFront.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.auton.paths;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.carriage.hatchclaw.Release;
+import frc.robot.carriage.tilt.SetTiltPosition;
+import frc.robot.carriage.tilt.Tilt;
+import frc.robot.climberelevator.powerpack.PowerPack;
+import frc.robot.climberelevator.powerpack.SetElevatorPosition;
+import frc.robot.drivetrain.commands.DriveStraight;
+import frc.robot.sensors.camera.FollowVision;
+
+public class RightRocketFront extends CommandGroup {
+
+  /**
+   * Place a Hatch on the right rocket close side, starting from right side of hab 1
+   */
+  public RightRocketFront() {
+
+    // Set Tilt and Elevator
+    addParallel(new SetTiltPosition(Tilt.MIDDLE));
+    addParallel(new SetElevatorPosition(PowerPack.LOWER_HATCH));
+
+    // Drive to the Rocket
+    addSequential(new DriveStraight(184, .4, 28.75)); //TODO: after testing, add ramping and tune values
+
+    // Follow vision into the rocket and score hatch
+    addSequential(new FollowVision(), 3000);  //TODO: make shorter after testing
+    addSequential(new Release());
+
+    // Back out
+    addSequential(new DriveStraight(-20, -.2));
+
+  }
+}
