33f2c638a40f092ed041c113c1b85050cb325379
tornado702 <ryansforrest@gmail.com>
2019-04-19 11:18:54 -0400

    created right rocket 2 hatch auton path

---
+++ src/main/java/frc/robot/auton/paths/RightRocketFrontDouble.java
@@ -0,0 +1,66 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.auton.paths;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.RobotMap.ClimberElevator.Powerpack;
+import frc.robot.carriage.hatchclaw.Grab;
+import frc.robot.carriage.hatchclaw.Release;
+import frc.robot.carriage.tilt.SetTiltPosition;
+import frc.robot.carriage.tilt.Tilt;
+import frc.robot.climberelevator.powerpack.PowerPack;
+import frc.robot.climberelevator.powerpack.SetElevatorPosition;
+import frc.robot.drivetrain.commands.DriveStraight;
+import frc.robot.drivetrain.commands.TurnByAngleGyro;
+import frc.robot.sensors.camera.FollowVision;
+
+public class RightRocketFrontDouble extends CommandGroup {
+
+  /**
+   * Place hatch on right front rocket, get hatch from loading station, place hatch on 2nd level right front rocket
+   */
+  public RightRocketFrontDouble() {
+    
+    // Set Tilt and Elevator
+    addParallel(new SetTiltPosition(Tilt.MIDDLE));
+    addParallel(new SetElevatorPosition(PowerPack.LOWER_HATCH));
+
+    // Drive to the Rocket
+    addSequential(new DriveStraight(184, .4, 28.75)); //TODO: after testing, add ramping and tune values
+
+    // Follow vision into the rocket and score hatch
+    addSequential(new FollowVision(), 3000);  //TODO: make shorter after testing
+    addSequential(new Release());
+
+    // Back out and turn towards player station
+    addSequential(new DriveStraight(-50, -.2, 150));
+
+    // Drive to player Station
+    addSequential(new DriveStraight(200, .4));
+
+    // Follow vision into player station and grab hatch
+    addSequential(new FollowVision(), 3000);
+    addSequential(new Grab());
+
+    // Back out and turn
+    addSequential(new DriveStraight(-50, -.2));
+    addSequential(new TurnByAngleGyro(180));
+
+    // Drive to rocket and set elevator height
+    addSequential(new DriveStraight(180, .4));
+    addSequential(new SetElevatorPosition(PowerPack.MIDDLE_HATCH));
+
+    // Follow Vision and score hatch
+    addSequential(new FollowVision(), 3000);
+    addSequential(new Release());
+
+    // Back out
+    addSequential(new DriveStraight(-30, -.2));
+
+  }
+}
